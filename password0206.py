d1406dba2b279bd50095ecf0aa8b579ab3f463ee
parksontyc <parkson.tyc.chen@gmail.com>
2021-02-06 12:16:53 +0800

    upload

--- password0206.py
+++ password0206.py
@@ -5,19 +5,19 @@
 #不對的話，印出"密碼錯誤，還有__次機會"
 #對的話，印出"登入成功"
 
-user_password = input('請輸入密碼：')
-
-while True:
-	
-	if user_password == 'a1234':
-		print('登入成功')
+password = '123'
+i = 3
+while i > 0 :
+	pwd = input('請輸入密碼：')
+	i = i - 1
+	if pwd == password:
+		print('登入成功！')
 		break
-	elif user_password != 'a1234':
-		x = x - 1
-		print('密碼錯誤，還有'+ str(x) +'次機會！')
-	elif x == 0:
-		print('密碼錯誤，請洽客服專員。')
 	else:
-		break
+		print("密碼錯誤。")
+		if i > 0:
+			print("還有", i, "次機會。")
+		else:
+			print('請洽客服')
 
 
